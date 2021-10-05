6c3a5c8c3971d8e6f5ba609c9a1f22d12ef47e1b
henrytjj <45747277+henrytjj@users.noreply.github.com>
2021-10-05 11:26:11 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 10:41:45 2021
+
+@author: hteh
+"""
+
+import streamlit as st,pandas as pd,numpy as np,os,pickle
+
+
+st.title('Henry Testing')
+st.subheader('Raw data')
+data=pd.read_csv('vc1902_final.csv').dropna(axis=1,thresh=30000).head(200)
+st.write(data)
